9d82e45e00f8602c6eb48eec064fc1c0c4f22a9f
unknown <zahrabonyfa@gmail.com>
2024-10-05 20:12:39 +0700

    first commit

---
+++ Pratikum_01/src/Tes_koneksi/Latihan2.java
@@ -0,0 +1,35 @@
+
+
+package Tes_koneksi;
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author PC 2
+ */
+public class Latihan2 {
+    static final String DB_Url = "jdbc:mysql://localhost:3306/db_unidha";
+    private static Connection Test_Konek;
+    public static Connection DB_Konek() throws SQLException, ClassNotFoundException {
+        try {
+            Driver myDriver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(myDriver);
+            System.out.println("proses Deteksi Driver Berhasil");
+            final java.sql.Connection tersambung = DriverManager.getConnection(DB_Url, "root", "");
+            System.out.println("Koneksi Database Berhasil");
+        } catch (final SQLException ex) {
+            System.out.println("Koneksi Database Gagal");
+        }
+        return Test_Konek;
+     
+            
+    }
+    public static void main(String[]args) {
+    }
+}
+    
+    
+
